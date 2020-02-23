d28ad289873aeefb9c9e6e3f767d8f658eabf4be
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-02-22 17:55:39 -0800

    added in the meow sounds dude

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,62 @@
 import { useSharedState, RoomServiceProvider } from "@roomservice/react";
 import React from "react";
+import Sound from "react-sound";
 
 const App = () => {
   const [sharedState, setSharedState] = useSharedState("my-room");
 
   function onClick() {
     setSharedState(prevDoc => {
-      prevDoc.number = Math.floor(Math.random() * 100);
+      if (!prevDoc.array || !Array.isArray(prevDoc.array)) {
+        prevDoc.array = [1];
+      }
+      prevDoc.array = prevDoc.array.concat([1]);
     });
   }
+
+  function arrReduce() {
+    setSharedState(prevDoc => {
+      prevDoc.array.pop();
+    });
+  }
+  const mappedMeows = (sharedState.array || []).map(function(item) {
+    return (
+      <Sound
+        url="http://localhost:3000/meow.mp3"
+        playStatus={"PLAYING"}
+        playFromPosition={0}
+        ignoreMobileRestrictions={true}
+      />
+    );
+  });
 
   return (
     <div>
       <h1>Open multiple browser windows!</h1>
 
-      <p>{sharedState.number || 0}</p>
+      <p>{sharedState.array || [1]}</p>
 
-      <button onClick={onClick}>Pick Random Number</button>
+      <button onClick={onClick}>Increase Array</button>
+      <button onClick={arrReduce}>Decrease Array</button>
+      {mappedMeows}
+      <Sound
+        url="http://localhost:3000/meow.mp3"
+        playStatus={"PLAYING"}
+        playFromPosition={0}
+        ignoreMobileRestrictions={true}
+      />
+      <Sound
+        url="http://localhost:3000/meow.mp3"
+        playStatus={"PLAYING"}
+        playFromPosition={300}
+        ignoreMobileRestrictions={true}
+      />
+      <Sound
+        url="http://localhost:3000/meow.mp3"
+        playStatus={"PLAYING"}
+        playFromPosition={600}
+        ignoreMobileRestrictions={true}
+      />
     </div>
   );
 };
